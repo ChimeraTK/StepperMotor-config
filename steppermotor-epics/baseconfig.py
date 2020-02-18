7fa9a86cd5d64c2ae54f7d475ee66420c4bcd8c9
Nadeem <nadeem.shehzad@desy.de>
2020-02-18 16:51:09 +0100

    steppermotor-epics moved to mako.

---
+++ steppermotor-epics/baseconfig.py
@@ -0,0 +1,9 @@
+(ACCELERATOR,STATION)=INSTANCE_CONFIG
+FULL_STATION_NAME= ("A" if "${ACCELERATOR}" == "XFEL" else "ACC" if "${ACCELERATOR}" == "FLASH" else "") + STATION
+
+SERVERNAME=STATION
+EXECUTABLE_IN_PACKAGE="/usr/bin/steppermotorserver"
+WORKDIR="/var/epics-servers/"+SERVERNAME
+FILES_TO_SYMLINK_BETWEEN_INSTANCES=""
+MAKE_EXECUTABLE="iocBoot/iocChimeraTKApp/st.cmd iocBoot/iocChimeraTKApp/initMotorDriverHW.py"
+CYCLE_TIME_MS=1000
---
+++ steppermotor-epics/settings/DEVEL.SHOWCASE.M1.py
@@ -0,0 +1,21 @@
+if ACCELERATOR == "DEVEL.SHOWCASE" :
+  if STATION == "M1" :
+
+    MAPFILE_BASENAME="controller_pzt4_unio_md22_fmc25_70t_r"
+
+    MOTORS=[1]
+    MOTORDRIVERS=1
+
+    MOTORDRIVER_SLOT=[4]
+    MOTORDRIVER_NAME=["MotorDriver1"]
+
+    MOTOR_DRIVER=[MOTORDRIVER_NAME[0]]
+    MOTOR_DRIVERTYPE=["MD22"]
+    MOTOR_TYPE=["LinearMotorWithReferenceSwitch"]
+    MOTOR_MODULENAME=["MD22.0"]
+    MOTOR_DRIVERID=[0]
+    MOTOR_MOTORDRIVERCONFIG=["Limes122-MotorDriverCardConfig.xml"]
+    MOTOR_MOTORSTEPSRATIO=[0.0003125]
+    #MOTOR_ENCODERSTEPSRATIO giving as string otherwise python does scientific converstion.
+    MOTOR_ENCODERSTEPSRATIO=["0.000001"]
+    MOTOR_POSITIONUNIT=["mm"]
